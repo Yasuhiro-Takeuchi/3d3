086e1fb8a0e30d4ce5c6e1a484007db36bbf3989
Yasuhiro-Takeuchi <21j5081@stu.meisei-u.ac.jp>
2023-06-22 20:10:00 +0900

    toriに変更

--- main.js
+++ main.js
@@ -14,7 +14,6 @@ document.addEventListener('DOMContentLoaded', () => {
     scene.add(light);
 
     const house = await loadGLTF('./assets/models/house/tori.gltf');
-    // const house = await loadGLTF('./assets/models/house/usi.gltf');
     house.scene.scale.set(0.3, 0.3, 0.3);
     house.scene.position.set(0, -1, 0.2);
     house.scene.rotation.set(0, 0, 90);
