c980bf0bbb1116bd7066d3a98c2cad389b51603d
Yasuhiro-Takeuchi <21j5081@stu.meisei-u.ac.jp>
2023-06-22 19:58:43 +0900

    scaleを0.3に変更

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ document.addEventListener('DOMContentLoaded', () => {
     scene.add(light);
 
     const house = await loadGLTF('./assets/models/house/usi.gltf');
-    house.scene.scale.set(3, 3, 3);
+    house.scene.scale.set(0.3, 0.3, 0.3);
     house.scene.position.set(0, -1, 0.2);
     house.scene.rotation.set(0, 0, 0);
 
