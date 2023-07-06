e6f1681c961164c181319189ba053baafa2936bd
Yasuhiro-Takeuchi <21j5081@stu.meisei-u.ac.jp>
2023-07-06 16:59:16 +0900

    マルチマーカー完成

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const start = async() => {
     const mindarThree = new window.MINDAR.IMAGE.MindARThree({
       container: document.body,
-      imageTargetSrc: './assets/targets/open2.mind'
+      imageTargetSrc: './assets/targets/test.mind'
     });
     const {renderer, scene, camera} = mindarThree;
 
@@ -13,20 +13,20 @@ document.addEventListener('DOMContentLoaded', () => {
     scene.add(light);
 
     const usi = await loadGLTF('./assets/models/house/usi.gltf');
-    const sakana = await loadGLTF('./assets/models/house/usi.gltf');
+    const dog = await loadGLTF('./assets/models/house/dog.gltf');
     usi.scene.scale.set(1, 1, 1);
     usi.scene.position.set(0, -1, 0.2);
     usi.scene.rotation.set(0, 0, 0);
 
-    sakana.scene.scale.set(1, 1, 1);
-    sakana.scene.position.set(0, -1, 0.2);
-    sakana.scene.rotation.set(0, 0, 0);
+    dog.scene.scale.set(1, 1, 1);
+    dog.scene.position.set(0, -1, 0.2);
+    dog.scene.rotation.set(0, 0, 0);
 
     const usiAncor = mindarThree.addAnchor(0);
     const sakanaAncor = mindarThree.addAnchor(1);
 
     usiAncor.group.add(usi.scene);
-    sakanaAncor.group.add(sakana.scene);
+    sakanaAncor.group.add(dog.scene);
 
     await mindarThree.start();
     renderer.setAnimationLoop(() => {
