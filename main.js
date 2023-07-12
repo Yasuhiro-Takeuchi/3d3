c94b7407c2e42cb7c5acf9db1b558ce3c3f97707
Yasuhiro-Takeuchi <21j5081@stu.meisei-u.ac.jp>
2023-07-12 19:33:12 +0900

    usi反応しない

--- main.js
+++ main.js
@@ -5,34 +5,55 @@ document.addEventListener('DOMContentLoaded', () => {
   const start = async () => {
     const mindarThree = new window.MINDAR.IMAGE.MindARThree({
       container: document.body,
-      imageTargetSrc: './assets/targets/sakana4.mind'
+      imageTargetSrc: './assets/targets/targets.mind'
     });
     const { renderer, scene, camera } = mindarThree;
 
     const light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
     scene.add(light);
 
-    const usi = await loadGLTF('./assets/models/house/tori.gltf');
-    const sakana = await loadGLTF('./assets/models/house/sakana.gltf');
+    const model0 = await loadGLTF('./assets/models/house/sakana.gltf');
+    const model1 = await loadGLTF('./assets/models/house/usi.gltf');
+    const model2 = await loadGLTF('./assets/models/house/hituzi.gltf');
+    const model3 = await loadGLTF('./assets/models/house/tori.gltf');
+    const model4 = await loadGLTF('./assets/models/house/sakana2.gltf');
     
 
-    usi.scene.scale.set(0.1, 0.1, 0.1);
-    usi.scene.position.set(0, 0.5, 0);
-    usi.scene.rotation.set(90, 0, 0);
+    model0.scene.scale.set(0.1, 0.1, 0.1);
+    model0.scene.position.set(0, 0.5, 0);
+    model0.scene.rotation.set(90, 0, 0);
 
-    sakana.scene.scale.set(0.1, 0.5, 0.5);
-    sakana.scene.position.set(0, 0, 0);
-    sakana.scene.rotation.set(0, 90, 0);
+    model1.scene.scale.set(0.1, 0.5, 0.5);
+    model1.scene.position.set(0, 0, 0);
+    model1.scene.rotation.set(0, 90, 0);
 
-    const usiAncor = mindarThree.addAnchor(0);
-    const sakanaAncor = mindarThree.addAnchor(1);
+    model2.scene.scale.set(0.1, 0.1, 0.1);
+    model2.scene.position.set(0, 0.5, 0);
+    model2.scene.rotation.set(90, 0, 0);
 
-    usiAncor.group.add(usi.scene);
-    sakanaAncor.group.add(sakana.scene);
+    model3.scene.scale.set(0.1, 0.5, 0.5);
+    model3.scene.position.set(0, 0, 0);
+    model3.scene.rotation.set(0, 90, 0);
+
+    model4.scene.scale.set(0.1, 0.5, 0.5);
+    model4.scene.position.set(0, 0, 0);
+    model4.scene.rotation.set(0, 90, 0);
+
+    const model0Ancor = mindarThree.addAnchor(0);
+    const model1Ancor = mindarThree.addAnchor(1);
+    const model2Ancor = mindarThree.addAnchor(2);
+    const model3Ancor = mindarThree.addAnchor(3);
+    const model4Ancor = mindarThree.addAnchor(4);
+
+    model0Ancor.group.add(model0.scene);
+    model1Ancor.group.add(model1.scene);
+    model2Ancor.group.add(model2.scene);
+    model3Ancor.group.add(model3.scene);
+    model4Ancor.group.add(model4.scene);
 
     //animation1
-    const mixer = new THREE.AnimationMixer(usi.scene);
-    const action = mixer.clipAction(usi.animations[0]);
+    const mixer = new THREE.AnimationMixer(model1.scene);
+    const action = mixer.clipAction(model1.animations[0]);
     action.play();
 
     const clock = new THREE.Clock();
